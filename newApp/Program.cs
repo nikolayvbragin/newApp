2644affe5f6c2672daad3b128fb9019a37ca02f1
nikolayvbragin <nik_bragin@mail.ru>
2021-03-23 21:43:49 +0700

    Добавьте файлы проекта.

---
+++ newApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Ваше имя:");
+            string s = Console.ReadLine();
+            Console.WriteLine("Введено:" + s);
+            Console.WriteLine("Нажмите любую клавишу для выхода");
+            Console.Read();
+            Console.Clear();
+        }
+    }
+}
